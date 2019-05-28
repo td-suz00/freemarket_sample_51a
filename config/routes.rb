a47bf69100a1dac52e7549896fd516235e7266d5
Miho Hamanaka <2081050.nono@gmail.com>
2019-05-28 18:41:34 +0900

    markup_item_registered_page_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ get   'items/new'  =>  'items#new'
+#  resources :items, only: [:new]
 end
