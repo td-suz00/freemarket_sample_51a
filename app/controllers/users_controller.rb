d4ac70f8c7e8ace8124fd29b08f57521aad4924b
td-suz00 <b0651118@gmail.com>
2019-05-29 12:26:46 +0900

    ユーザープロフィール編集画面のマークアップ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  
   def show
   end
+
+  def edit
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'items#index'
+  root 'users#edit'
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit]
 
 end
