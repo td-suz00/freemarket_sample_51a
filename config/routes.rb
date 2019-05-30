ee054049252f5d7e9a973c6f0dfc8392a3c94d0c
Miho Hamanaka <2081050.nono@gmail.com>
2019-05-30 15:19:06 +0900

    Mod_markup credit card view

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,16 @@
 Rails.application.routes.draw do
- get  'items/new'  =>  'items#new'
- get  'cards/index'  =>  'cards#index'
- get  'cards/new'  =>  'cards#new'
-
+root 'items#index'
+resources :items, only: [:new]
+resources :users, only: [:show] do
+    resources :cards, only: [:index, :new]
+end
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    sessions: 'users/sessions'   
-  } 
+    sessions: 'users/sessions'
+  }
 
   devise_scope :user do
     get "sign_in", to: "users/sessions#new"
-    get "sign_out", to: "users/sessions#destroy" 
+    get "sign_out", to: "users/sessions#destroy"
   end
-
-  root 'items#index'  
-  get   'items/new'  =>  'items#new'
-
-  resources :users, only: [:show]
-
 end
