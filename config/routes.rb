a444823cc48078b60925c4923a5ae82841165491
Yosuke <s7k315@gmail.com>
2019-05-30 18:41:46 +0900

    create purchases controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    sessions: 'users/sessions'   
-  } 
+    sessions: 'users/sessions'
+  }
 
   devise_scope :user do
     get "sign_in", to: "users/sessions#new"
-    get "sign_out", to: "users/sessions#destroy" 
+    get "sign_out", to: "users/sessions#destroy"
   end
 
   root 'items#index'
-
+  resources :purchases, only: :new
   resources :items, only: :new
   resources :users, only: :show do
     resources :logouts, only: :new
