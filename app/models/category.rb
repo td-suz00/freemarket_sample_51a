720349ad63627677481f5a67590c698e493595bf
Yosuke <s7k315@gmail.com>
2019-06-03 13:55:28 +0900

    mod option

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
-  has_many :children, class_name: "Category", foreign_key: "parent_id"
-  belongs_to :parent, class_name: "Category", foreign_key: "parent_id", optional: true, depentent: :destroy
+  has_many :children, class_name: "Category", foreign_key: "parent_id", depentent: :destroy
+  belongs_to :parent, class_name: "Category", foreign_key: "parent_id", optional: true
   has_many :items
   has_many :size_categories
   has_many :sizes, through: :size_categories
