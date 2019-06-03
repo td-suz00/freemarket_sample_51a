20ee0f949cba9d82c17305a086f8c3b16ed0c04e
Yosuke <s7k315@gmail.com>
2019-06-03 13:50:01 +0900

    fix assosiation
    brand
    item
    category
    item
    size
    size_category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,8 @@
 class Category < ApplicationRecord
-
+  has_many :children, class_name: "Category", foreign_key: "parent_id"
+  belongs_to :parent, class_name: "Category", foreign_key: "parent_id", optional: true, depentent: :destroy
   has_many :items
+  has_many :size_categories
+  has_many :sizes, through: :size_categories
 
-  has_many :children, class_name: "Category", foreign_key: "parent_id", dependent: :destroy
-  belongs_to :parent, class_name: "Category", foreign_key: "parent_id", optional: true
-  
 end
--- app/models/size.rb
+++ app/models/size.rb
@@ -1,2 +1,6 @@
 class Size < ApplicationRecord
+  has_many :items
+  has_many :size_categories
+  has_many :categories, through: :size_categories
+
 end
