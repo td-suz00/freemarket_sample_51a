73c8b630da01fa581f2804aa28d89479a11cf9ae
Yosuke <s7k315@gmail.com>
2019-06-03 13:09:45 +0900

    mod user_item assosiation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :buyer_items, class_name: 'Item', foreign_key: 'buyer_id'
-  has_many :seller_items, class_name: 'Item', foreign_key: 'seller_id'
+  has_many :buyed_items, foreign_key: :buyer_id, class_name: :Item
+  has_many :selling_items, -> { where("buyer_id is NULL") }, foreign_key: :seller_id, class_name: :Item
+  has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: :seller_id, class_name: :Item
 
   validates :nickname, presence: true, exclusion: { in: %w(メルカリ) }
   validates :email, presence: true, uniqueness: true
