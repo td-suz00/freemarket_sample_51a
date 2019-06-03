b9a615c6293d5a468e5f7ed1746b4db8ae38daa4
Miho Hamanaka <2081050.nono@gmail.com>
2019-06-03 13:18:36 +0900

    Mod_markup_signup_view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,7 @@
 class UsersController < ApplicationController
-  def index
-  end
-
   def new
   end
 
   def show
-  end
-
-  def create
   end
 end
--- spec/controllers/signups_controller_spec.rb
+++ spec/controllers/signups_controller_spec.rb
@@ -1,5 +1,3 @@
-require 'rails_helper'
-
 RSpec.describe SignupsController, type: :controller do
 
 end
