b0b547f72d325441d8070b03889b4eb56f3a0f02
Miho Hamanaka <2081050.nono@gmail.com>
2019-06-05 15:11:46 +0900

    markup_item_detail_in brief
    商品画面のみ完成

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,4 +2,7 @@ class PurchasesController < ApplicationController
   def new
     render layout: 'application-off-header-footer.haml'
   end
+  def show
+    render layout: 'application-off-header-footer.haml'
+  end
 end
