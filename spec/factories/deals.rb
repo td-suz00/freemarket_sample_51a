ca86c372032de8d7ce3f151d3ddb290fe093f4a0
Yosuke <s7k315@gmail.com>
2019-06-05 18:58:41 +0900

    implement model tests
    Card
    Deal

---
+++ spec/factories/deals.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+
+  factory :deal do
+    charge_id        {"ch_238b60d543f265732027d5e20ada8"}
+    deal_at          {"2019-06-05 06:30:05"}
+    status_id        {2}
+    buyer_id         {2}
+    seller_id        {3}
+    item_id          {5}
+  end
+
+end
