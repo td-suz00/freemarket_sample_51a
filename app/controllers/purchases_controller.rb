e0de3cff5ece409d803efe835dcf29fb464f0552
Yosuke <s7k315@gmail.com>
2019-06-05 10:37:05 +0900

    mod purchase_confirmation view

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,7 @@
 class PurchasesController < ApplicationController
   def new
+    @item = Item.find(4)
+    #### 仮置き Item.find(params[:item_id])
     render layout: 'application-off-header-footer.haml'
   end
 end
