529a64eea5a480f90e4ffa3ab120290cc1f6023e
haruki <nagasoe0326@gmail.com>
2019-06-09 14:42:05 +0900

    create signup-success-view

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,14 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
+    get 'signup/successful' , to: 'signups#successful'
     post 'signup/sms_confirmation_certify' , to: 'signups#sms_confirmation_certify'
     patch 'signup/address/create' , to: 'signups#address_create'
     get 'signup/sms_confirmation' , to: 'signups#sms_confirmation_send'
     get 'signup/address' , to: 'signups#address'
+    
     get 'items/search_category' , to: 'items#search_category'
     get 'items/auto_complete' , to: 'items#auto_complete'
-    # devise_for :users, controllers: { registrations: 'users/registrations' }
   devise_scope :user do
     get "sign_in", to: "users/sessions#new"
     get "sign_out", to: "users/sessions#destroy"
@@ -20,16 +21,9 @@ Rails.application.routes.draw do
   end
 
   root 'top#index'
-
-  resources :top, only: :index
-  resources :items, only: [:new, :create, :edit, :update,:show] do
-    resources :purchases, only: :new do
-      collection do
-        post 'pay', to: 'purchases#pay'
-      end
-    end
-  end
-
+  resources :top, only: [:index]
+  resources :purchases, only: :new
+  resources :items, only: [:new, :create, :edit, :update]
   resources :users, only: [:new, :show] do
     resources :cards, only: [:index, :new, :show, :destroy] do
       collection do
@@ -37,10 +31,8 @@ Rails.application.routes.draw do
         post 'pay', to: 'cards#pay'
       end
     end
-    #### :showは仮置き。signup#doneなどのアクションにあてるのが良いか
     resources :user_profiles, only: [:new, :create, :edit, :update]
     resources :user_confirmations, only: [:create, :edit, :update]
     resources :logouts, only: :new
-    resources :signups, only: [:index, :new, :show]
   end
 end
