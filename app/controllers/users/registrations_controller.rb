f986c3422e34f83e619d1c30453d4e30cab5f11a
haruki <nagasoe0326@gmail.com>
2019-06-11 09:47:20 +0900

    fix for code review

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,7 +5,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 def new
   @user = User.new
-  @profile = @user.build_profile　  
+  @profile = @user.build_profile
 end
 
   # GET /resource/sign_up
