213235169837e5e43074f77ef7e4fb179300c5b4
haruki <nagasoe0326@gmail.com>
2019-06-12 17:42:19 +0900

    create  model sns_credential

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_06_074753) do
+ActiveRecord::Schema.define(version: 2019_06_12_084046) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -118,6 +118,15 @@ ActiveRecord::Schema.define(version: 2019_06_06_074753) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "statuses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -148,4 +157,5 @@ ActiveRecord::Schema.define(version: 2019_06_06_074753) do
   add_foreign_key "profiles", "users"
   add_foreign_key "size_categories", "categories"
   add_foreign_key "size_categories", "sizes"
+  add_foreign_key "sns_credentials", "users"
 end
