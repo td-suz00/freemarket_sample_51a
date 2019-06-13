ffcb37d889bf02c2a56ac9aa15685d13332ccaeb
Yosuke <s7k315@gmail.com>
2019-06-13 18:03:09 +0900

    mod_dummy_path

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,8 +21,7 @@ class ItemsController < ApplicationController
           @item.item_images.create(image_url: image, item_id: @item.id)
         end
       end
-      Deal.create(seller_id:1,item_id:@item.id, status_id:1)
-      ####仮置き   正：Deal.create(seller_id:current_user,item_id:@item.id, status_id:1)
+      Deal.create(seller_id: current_user.id ,item_id: @item.id, status_id:1)
       redirect_to root_path
     else
       @item.item_images.build
@@ -70,7 +69,6 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :text, :category_id, :size_id, :brand_id, :condition, :delivery_fee_payer, :delivery_type, :delibery_from_area, :delivery_days, :price, item_images_attributes: [:id, :image_url, :item_id])
-    #### ログイン機能ができたら.merge(seller_id: current_user.id)
   end
 
 end
