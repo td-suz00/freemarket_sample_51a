c985616da412e230e051466853a54e22c1a2dbe3
haruki <nagasoe0326@gmail.com>
2019-06-14 17:01:05 +0900

    create validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth, if: :production?
-
+  before_action :check_login
   
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,profile_attributes: [:family_name,:last_name,:kana_family_name,:kana_last_name,:birth_ymd,:user_id]])
@@ -13,7 +13,13 @@ class ApplicationController < ActionController::Base
   def production?
     Rails.env.production?
   end
-
+    def check_login
+    if current_user && current_user.profile.postalcode.nil?
+    redirect_to sign_out_path 
+    current_user.destroy
+    end
+  end
+  
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == Rails.application.credentials.production[:basic_auth_user] && password == Rails.application.credentials.production[:basic_auth_password]
