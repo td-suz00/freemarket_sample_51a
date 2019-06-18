a0e9216e9089a96d72ddc94028552bacbc43292f
Yosuke <s7k315@gmail.com>
2019-06-18 17:30:26 +0900

    impl card info

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -11,4 +11,21 @@ module CardsHelper
     end
     view_exp_month + " / " + info.exp_year.to_s[2, 2]
   end
+
+  def get_cards_brand_logo(brand)
+    case brand
+      when 'Visa'
+        image_tag 'visa-logo.jpg',size: "47x23" , class: "credit__logo-img"
+      when 'JCB'
+        image_tag 'jcb-logo.svg',size: "34x23" , class: "credit__logo-img"
+      when 'MasterCard'
+        image_tag 'mastercard-logo.svg',size: "27x23" , class: "credit__logo-img"
+      when 'American Express'
+        image_tag 'amex-logo.gif',size: "23x23" , class: "credit__logo-img"
+      when 'Diners Club'
+        image_tag 'diners-logo.gif',size: "27x23" , class: "credit__logo-img"
+      when 'Discover'
+        image_tag 'discover-logo.gif',size: "27x23" , class: "credit__logo-img"
+    end
+  end
 end
