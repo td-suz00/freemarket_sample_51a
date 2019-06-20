e170a23eff5a1b8fa0c5634a756c47f8c919605d
Yosuke <s7k315@gmail.com>
2019-06-20 12:07:49 +0900

    create set_item

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,11 +1,11 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item, only: [:new, :pay, :address, :card, :card_index]
 
   require 'payjp'
   require 'date'
 
   def new
-    @item = Item.find(params[:item_id])
     @card = current_user.card
     if @card
       Payjp.api_key = Rails.application.credentials.payjp[:test_secret_key]
@@ -16,13 +16,12 @@ class PurchasesController < ApplicationController
   end
 
   def pay
-    item = Item.find(params[:item_id])
     card = Card.find_by(user_id: current_user.id)
-    deal = Deal.find_by(item_id: item.id)
+    deal = Deal.find_by(item_id: @item.id)
     if card
       Payjp.api_key = Rails.application.credentials.payjp[:test_secret_key]
       charge = Payjp::Charge.create(
-        amount:   item.price,
+        amount:   @item.price,
         customer: card.customer_id,
         currency: 'jpy',
       )
@@ -35,23 +34,20 @@ class PurchasesController < ApplicationController
       redirect_to controller: :users, action: :show, id: current_user.id
     else
       flash[:alert] = 'カード情報を登録してください'
-      redirect_to controller: :purchases, action: :new, item_id: item.id
+      redirect_to controller: :purchases, action: :new, item_id: @item.id
     end
   end
 
   def address
-    @item = Item.find(params[:item_id])
     render layout: 'application-off-header-footer.haml'
   end
 
   def card
-    @item = Item.find(params[:item_id])
     gon.payjp_test_pk = Rails.application.credentials.payjp[:test_public_key]
     render layout: 'application-off-header-footer.haml'
   end
 
   def card_index
-    @item = Item.find(params[:item_id])
     @card = current_user.card
     if @card
       Payjp.api_key = Rails.application.credentials.payjp[:test_secret_key]
@@ -59,5 +55,11 @@ class PurchasesController < ApplicationController
       @default_card_information = customer.cards.retrieve(@card.card_id)
     end
     render layout: 'application-off-header-footer.haml'
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
